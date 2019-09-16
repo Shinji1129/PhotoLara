6d9a436568679e19ca2392fb1b48dc51acf1e0d6
Shinji Kawamoto <viii49ix40x35@gmail.com>
2019-09-17 06:34:42 +0900

    一覧表示機能スタイル

--- resources/views/pages/list.blade.php
+++ resources/views/pages/list.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.layout')
 @section('content')
 <div class="list">
-  @foreach($photos as $photo)
-  <h1>Photo List</h1>
-  <p>{{ $photo->title }}</p>
-  @if ($photo->file)
-<img src="{{ asset('/storage/'.$photo->file) }}" alt="">
-@endif
+@foreach($photos as $photo)
+  <div class="list--wrap">
+    <div class="list--item">
+      <div class="list--over-ray"></div>
+        <p class="list--title">{{ $photo->owner->name }}</p>
+        <img src="{{ asset('/storage/'.$photo->file) }}" class="list--image">
+        <p class="list--like list--icon"><i class="bg fas fa-heart"></i>10</p>
+        <p class="list--download list--icon"><i class="bg fas fa-cloud-download-alt"></i>download</p>
+    </div>
+  </div>
   @endforeach
 </div>
 @endsection
